9b4dd844149072fb038e25248508ab7fb5aecec8
Jose Luis Ordiales Coscia <jlordiales@gmail.com>
2014-10-31 23:56:03 -0200

    Trying to override datasource config by environment

---
+++ server/datasources.production.js
@@ -0,0 +1,12 @@
+var db_url = process.env.MONGOHQ_URL
+
+var config = {
+  "url": db_url
+  "database": "comida",
+  "name": "db",
+  "connector": "loopback-connector-mongodb"
+}
+
+module.exports = {
+  db: config
+};
